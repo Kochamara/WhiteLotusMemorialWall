def78e44f7ea11cb726d8b5050d317a4a83a227f
Kochamara <spencerguy25@gmail.com>
2025-02-20 13:27:22 -0500

    enable GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import plugin from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/WhiteLotusMemorialWall/',
     plugins: [plugin()],
     server: {
         port: 57499,
